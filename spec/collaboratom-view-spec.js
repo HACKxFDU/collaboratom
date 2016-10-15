bd4191c21735945a51dd2961c0589d54c9444717
dragonly <liyilongko@gmail.com>
2016-10-15 10:39:22 +0800

    [Add] chat panel

---
+++ spec/collaboratom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CollaboratomView from '../lib/collaboratom-view';
+
+describe('CollaboratomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
